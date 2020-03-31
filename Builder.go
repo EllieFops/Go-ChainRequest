1decc6da42755a9dd6edf26704983e2b9bfe03c0
Elizabeth Harper <epharper@upenn.edu>
2020-03-31 12:07:33 -0400

    Added request builder function

--- Builder.go
+++ Builder.go
@@ -7,3 +7,9 @@ import (
 type RequestBuilder interface {
 	Build(request Request) (*http.Request, error)
 }
+
+type RequestBuilderFunc func(request Request) (*http.Request, error)
+
+func (r RequestBuilderFunc) Build(request Request) (*http.Request, error) {
+	return r(request)
+}
